1c1e9c99189a80f165ca9301e4ac9a27fb988490
myselfanandvp <mailanandvp@gmail.com>
2025-05-26 02:21:25 +0530

    add user function is in checking

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -11,6 +11,18 @@ def homepage(request):
     return render(request,'homepage.html',{})
 
 def loginpage(request):
+    if request.method=="POST":
+        email=request.POST["emailid"].strip().lower()
+        password= request.POST["password"].strip()
+        if User.objects.filter(username=email).exists():
+            return render(request,'loginpage.html',{"error":"User already exists"})
+        newuser=User(username=email,email=email)
+        newuser.set_password(password)
+        newuser.is_staff=True
+        newuser.save()
+        print("user has been saved")
+        return "User hasbeen saved"
+        
     return render(request,'login.html',{})
 
 
