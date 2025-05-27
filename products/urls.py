96da3aaf289a1eede37351a42aa5a185accc1def
myselfanandvp <mailanandvp@gmail.com>
2025-05-27 15:15:52 +0530

    product crud completed

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import product_create,product_list
+from .views import product_create,product_list,editproduct,delete_product
 
 urlpatterns=[    
     path("create/",product_create,name='createproduct'),
-    path("list/",product_list,name='productlist')
+    path("list/",product_list,name='productlist'),
+    path("edit/<int:product_id>/",editproduct,name='editproducturl'),
+    path("delete/<int:product_id>/",delete_product,name='deleteproducturl'),
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,23 +1,53 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404,get_list_or_404
+from django.http import HttpResponse
 from .forms import ProductForm
 from django.contrib.auth.decorators import login_required
 from .models import Products
+import os
+from django.conf import settings
 # Create your views here.
+
 
 @login_required
 def product_create(request):
     if request.POST:
-        newproduct = ProductForm(request.POST,request.FILES)
+        newproduct = ProductForm(request.POST, request.FILES)
         if newproduct.is_valid():
             newproduct.save()
             return redirect("productlist")
         print(newproduct.errors)
-        return render(request,'product_create.html',{'productform':ProductForm(request.POST)})
+        return render(request, 'product_create.html', {'productform': ProductForm(request.POST)})
 
-    return render(request,'product_create.html',{'productform':ProductForm})
+    return render(request, 'product_create.html', {'productform': ProductForm})
 
 
 @login_required
 def product_list(request):
     products=Products.objects.all()
-    return render(request,'productlist.html',{'products':products})
+    return render(request, 'productlist.html', {'products': products})
+
+
+@login_required
+def editproduct(request, product_id):
+    product = get_object_or_404(Products, id=product_id)
+    if request.method == "POST" and product:
+        updated_form = ProductForm(
+            request.POST, request.FILES, instance=product)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect("productlist")
+  
+    form = ProductForm(instance=product)
+
+    return render(request, 'product_edit.html', {'editform': form})
+
+
+@login_required
+def delete_product(request,product_id):
+    obj=get_object_or_404(Products,id=product_id)    
+    if obj.image:
+        image_path = os.path.join(settings.MEDIA_ROOT,obj.image.name)
+        if os.path.exists(image_path):
+            os.remove(image_path)
+    obj.delete()
+    return redirect("productlist")
